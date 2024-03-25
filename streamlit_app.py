850ad58a5bbedcf55a4aea5e025c842c7c793c70
Amalie Strømme Falck <86004779+amaliesf@users.noreply.github.com>
2024-03-25 11:42:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import snowflake.connector
 import requests
 from urllib.error import URLError
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Parents New Unhealthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
